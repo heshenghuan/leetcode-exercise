85aff5ea6a5e7b02a693d7e3a422e7ddcf9830ba
heshenghuan <heshenghuan@sina.com>
2016-07-31 15:34:36 +0800

    #279 modified, make it faster

--- 2016/July/279_Perfect_Squares.cpp
+++ 2016/July/279_Perfect_Squares.cpp
@@ -6,11 +6,14 @@ public:
      * dp[j] = min(dp[j], dp[j-i2]+1)
      */
     int numSquares(int n) {
-        static vector<int> dp {0};
+        static vector<int> dp {0}; // using static here to reduce time cost
         int m = dp.size();
-        dp.resize(max(m, n+1), INT_MAX);
+        dp.resize(max(m, n+1), INT_MAX); // resize函数只对新添加在末尾的元素赋值为INT_MAX，其他元素不变
         for(int i=1, i2; (i2 = i*i)<=n; ++i) {
-            for(int j=i2; j<=n; ++j) {
+            for(int j=max(m,i2); j<=n; ++j) {
+                // max(m, i2)很重要，能快很多倍
+                // 因为dp是static声明的，一直存在内存中，当重复调用时m会改变（增大）
+                // 若m>i2，则可以避免重复计算
                 if(dp[j] > dp[j-i2] + 1)
                     dp[j] = dp[j-i2] + 1;
             }
