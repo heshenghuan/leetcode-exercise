de6e651976d351ed8a5b4eb32fe616d2817dcbf2
heshenghuan <heshenghuan@sina.com>
2016-08-16 16:24:25 +0800

    Added some comments

--- 2016/August/10_Regular_Expression_Matching.cpp
+++ 2016/August/10_Regular_Expression_Matching.cpp
@@ -11,6 +11,7 @@ public:
         for(int i=1;i<=m;i++)
             dp[i][0] = false;
         for(int j=1;j<=n;j++)
+            // cause p[0] cannot be '*', so dp[0][1] = false
             dp[0][j] = j > 1 && p[j-1] == '*' && dp[0][j-2];
 
         for(int i=1;i<=m;i++) {
@@ -18,6 +19,7 @@ public:
                 if(p[j-1] != '*')
                     dp[i][j] = dp[i-1][j-1] && (s[i-1] == p[j-1] || '.' == p[j-1]);
                 else
+                    // p[0] cannot be '*', so no need to check "j > 1" here
                     dp[i][j] = dp[i][j-2] || (s[i-1] == p[j-2] || '.' == p[j-2]) && dp[i-1][j];
             }
         }
